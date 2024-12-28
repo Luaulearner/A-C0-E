0507588f26ddd8b50a9e08fbdf55c0918acc4ae3
Luaulearner <ppggyar@gmail.com>
2024-12-29 00:09:01 +0800

    update sevenhourwar.lua

---
+++ sevenhourwar.lua
@@ -0,0 +1,8 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("S.H.W", "DarkTheme")
+
+
+--• Main Sector
+
+local Main = Window:NewTab("Home")
+local MainSection = Main:NewSection("Section Name")
