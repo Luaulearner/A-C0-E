7384be6a20035f65b1d7897ef50b95697ef931f7
Luaulearner <ppggyar@gmail.com>
2024-12-29 05:28:12 +0800

    update reworkedshw.lua

--- reworkedshw.lua
+++ reworkedshw.lua
@@ -1,8 +1,8 @@
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/memejames/elerium-v2-ui-library//main/Library", true))()
 
 
-local Mainwindow = library:AddWindow("Home", {
-	main_color = Color3.fromRGB(173, 216, 230), -- Color
+local Mainwindow = library:AddWindow("_fpsboost", {
+	main_color = Color3.fromRGB(0, 0, 0), -- Color
 	min_size = Vector2.new(250, 346), -- Size of the gui
 	can_resize = true, -- true or false
 })
@@ -10,8 +10,11 @@ local Mainwindow = library:AddWindow("Home", {
 local features = Mainwindow:AddTab("Features") -- Name of tab
 features:Show() -- shows the tab
 
-features:AddButton("Do Not Press!",function()
-	print("why no listennbrother")
+features:AddButton("",function()
+	while true do
+	  task.wait(999999)
+	  print("yes bro")
+	end
 end)
 
 
