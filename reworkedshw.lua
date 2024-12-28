6fbe4177b0767409080bc1564e3f616b48ca5d68
Luaulearner <ppggyar@gmail.com>
2024-12-29 05:25:36 +0800

    update reworkedshw.lua

--- reworkedshw.lua
+++ reworkedshw.lua
@@ -1,13 +1,13 @@
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/memejames/elerium-v2-ui-library//main/Library", true))()
 
 
-local window = library:AddWindow("Home", {
-	main_color = Color3.fromRGB(41, 74, 41), -- Color
+local Mainwindow = library:AddWindow("Home", {
+	main_color = Color3.fromRGB(173, 216, 230), -- Color
 	min_size = Vector2.new(250, 346), -- Size of the gui
 	can_resize = true, -- true or false
 })
 
-local features = window:AddTab("Features") -- Name of tab
+local features = Mainwindow:AddTab("Features") -- Name of tab
 features:Show() -- shows the tab
 
 features:AddButton("Do Not Press!",function()
