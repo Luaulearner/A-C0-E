f816e62e6b8db5ec40616338bf305516a4c9843c
Luaulearner <ppggyar@gmail.com>
2024-12-30 01:29:30 +0800

    update oppression_tsevenhourwar.lua

--- oppression_tsevenhourwar.lua
+++ oppression_tsevenhourwar.lua
@@ -13,11 +13,49 @@ local features = window:AddTab("Home Sweet Home.") -- Name of tab
 features:Show() -- shows the tab
 
 
-features:AddButton("name",function()
-	-- Code here
+features:AddButton("Bai bai Map.",function()
+	for _, v in pairs(workspace.Map:GetDescendants()) do
+      if v:IsA("Decal") or v:IsA("Texture") or v:IsA("MeshPart") or v:IsA("Mesh") or v:IsA("BlockMesh") or v:IsA("BevelMesh") or v:IsA("CylinderMesh") or v:IsA("SpecialMesh") or v:IsA("FileMesh") or v:IsA("ParticleEmitter") or v:IsA("TriangleMeshPart") then
+        v:Destroy()
+      end
+    end
 end)
 
 
-features:AddButton("name",function()
-	-- Code here
+features:AddButton("Bai bai Lobby",function()
+	for _, v in pairs(workspace.Lobby:GetDescendants()) do
+      if v:IsA("Decal") or v:IsA("Texture") or v:IsA("MeshPart") or v:IsA("Mesh") or v:IsA("BlockMesh") or v:IsA("BevelMesh") or v:IsA("CylinderMesh") or v:IsA("SpecialMesh") or v:IsA("FileMesh") or v:IsA("ParticleEmitter") or v:IsA("TriangleMeshPart") then
+        v:Destroy()
+      end
+    end
+end)
+
+
+features:AddButton("Bai bai Props",function()
+  for _, v in pairs(workspace.MapScripts.Props:GetChildren()) do
+    v:Destroy()
+  end
+end)
+
+features:AddButton("So Bright!",function()
+	game.Lighting.Brightness = 20
+end)
+
+features:AddButton("Ooh, Sunny Ambient.",function()
+	game.Lighting.Ambient = Color3.fromRGB(255, 255, 255)
+end)
+
+local features = window:AddTab("Advanced") -- Name of tab
+features:Show() -- shows the tab
+
+
+features:AddTextBox("Any Gun GP.", function(text) -- u can add any text to "text"
+game:GetService("ReplicatedStorage").LoadoutGP:FireServer(text)
+end)
+
+
+features:AddTextBox("Teleport", function(usernameoftheguy) -- u can add any text to "text"
+	local a0 = game.Workspace[usernameoftheguy].HumanoidRootPart
+	local b0 = game.Players.LocalPlayer.Character.HumanoidRootPart
+	b0.CFrame = a0.CFrame
 end)
