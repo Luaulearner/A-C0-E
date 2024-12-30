0efcb94047649bb84afc5fac15ee39e1b91cced3
Luaulearner <ppggyar@gmail.com>
2024-12-30 23:15:22 +0800

    update gpl_reducer.lua

--- gpl_reducer.lua
+++ gpl_reducer.lua
@@ -12,6 +12,25 @@ task.wait(0.2)
 for _, decals in pairs(game:GetDescendants()) do
   if decals:IsA("Decal") or decals:IsA("Texture") then
     decals.Transparency = 1
+    decals.Shiny = 1
   end
 end
 task.wait(0.3)
+for _, parts in pairs(game:GetDescendants()) do
+  if parts:IsA("BasePart") or parts:IsA("Union") or parts:IsA("Part") then
+    parts.Reflectance = 0
+    parts.Material = "SmoothPlastic"
+  end
+end
+task.wait(0.4)
+for _, meshs in pairs(game:GetDescendants()) do
+  if meshs:IsA("DataModelMesh") or meshs:IsA("SpecialMesh") or meshs:IsA("Mesh") then
+    meshs.MeshId = ""
+  end
+end
+task.wait(0.5)
+for _, models in pairs(game:GetDescendants()) do
+  if models:IsA("Model") or models:IsA("Models") then
+    models.LevelOfDetail = 1
+  end
+end
